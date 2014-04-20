12f071cef66d74fcefbe4a33023234cfefc020ef
shinmuro <shinmuro@gmail.com>
2014-04-20 16:55:53 +0900

    9.3 まで完了

--- src/on_lisp/chapter-9.clj
+++ src/on_lisp/chapter-9.clj
@@ -13,44 +13,97 @@
 
 ;; p84
 
-;~~~~ 更新ここまで ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-"ここで挙げられている for はリスト内包表記な Clojure の for でなく、
-命令プログラミング的な for をマクロ実装してみたもの。"
-
-"Clojure でサンプルを素直に読み替えて定義してみた場合だと実際は使えない。"
-
-(defmacro for-a [[sym start stop] & body]
-  `(loop [~sym ~start
-          limit ~stop]
-     (when (<= ~sym limit)
-       ~@body
-       (recur (inc ~sym) limit))))
-
+"On Lisp で言う複数回の評価を起こし得るマクロが Clojure 版にあるかどうか
+良く分からん。形式的に近いのとなると for-b なんだが、"
 (defmacro for-b [[sym start stop] & body]
   `(loop [~sym ~start]
      (when (<= ~sym ~stop)
        ~@body
        (recur (inc ~sym)))))
 
-(defmacro for-c [[sym start stop] & body]
-  `(loop [~sym ~start
-          ~'limit ~stop]
-     (when (<= ~sym ~'limit)
-       ~@body
-       (recur (inc ~sym) ~'limit))))
+"特に問題ない。"
+(let [x 2]
+  (for-b [i 1 (inc x)]
+    (println i)))
+;=> 1
+;   2
+;   3
+;   nil
 
-(defmacro for-d [[sym start stop] & body]
-  `(let [b# (fn [~sym] ~@body)]
+"複数回評価を起こし得る状況を Clojure マクロで思いつかないので次へ。
+とは言え Clojure でも有り得るんじゃないかと気には留めておきたい。
+
+だた例示とは言え On Lisp のマクロ呼び出しで incf 使ってるのもあまり良くない。
+これは変数を変更しない 1+ を使えば延々と数字を print する現象は回避できる。
+
+まあ Mutable な変更を割と許している Common Lisp だとマクロ自体も
+あまりいい書き方じゃないんだろう。"
+
+
+;;;
+;;; 9.2 評価の順番
+;;;
+
+;; p84
+"これも一応 stop が先に評価されるようなマクロを定義してみたが、"
+(defmacro for-f [[sym start stop] & body]
+  `(let [gstop# ~stop]
      (loop [~sym ~start]
-       (when (<= ~sym ~stop)
-         (b# ~sym)
+       (when (<= ~sym gstop#)
+         ~@body
          (recur (inc ~sym))))))
 
-(defmacro for-e [[sym start stop] & body]
-  (let [gstop (gensym)]
-    `(loop [~sym ~start
-            ~gstop ~stop]
-       (when (<= ~sym ~gstop)
-         ~@body
-         (recur (inc ~sym) ~gstop)))))
+"問題ない。"
+(let [x 1]
+  (for-f [i x (+ x 13)]
+     (println i)))
+;=> 1
+;   2
+;   ...
+;   13
+;   nil
+
+"まあ急いで書いてる時にはこんな書き方するかもしれないから
+注意はしておいた方がいいかな。
+ただ Clojure 的な感覚から言うと破壊的変更をデフォルトで許している事も
+やっぱり問題な気がする。"
+
+;;;
+;;; 9.3 関数によらないマクロ展開
+;;;
+
+;; p85
+
+"Clojure でも誤った定義できると言えばできる。alter-var-root とか初めて使ったけど。"
+(def ^:dynamic *nil!s* 0)
+
+(defmacro nil! [x]
+  (alter-var-root (var *nil!s*) (partial inc))
+  `(def ~x nil))
+;=> #'user/nil!
+(nil! a)
+;=> #'user/a
+*nil!s*
+;=> 1
+
+"文字列での呼び出し。"
+(defmacro string-call [opstring & args]
+  `(~(symbol opstring) ~@args))
+;=> #'user/string-call
+(defn our+ [x y] (+ x y))
+;=> #'user/our+
+(string-call "clojure.core/our+" 2 3)
+;=> 5
+
+"いずれにせよあまり使う方法ではないと思う。
+
+この後 nconc(破壊的 concat) を利用したマクロにおいての危険性についての例示がある。
+Clojure だとありそうなのは transient 使った時かもしれないが、自分は使わないので
+読み流すだけにした。"
+
+;;;
+;;; 9.4 再帰
+;;;
+
+
+;~~~~ 更新ここまで ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
